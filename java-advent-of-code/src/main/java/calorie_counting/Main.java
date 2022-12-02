2329dbb2e6d02a4c0b9600328cdfaef4c13e5066
markusjohanson <johanssonmarkus1994@gmail.com>
2022-12-02 11:56:35 +0100

    Didn't think I would bother much about day 1, but checked out a collegues solution and it was no much cleaner I had to refactor mine accordingly

--- java-advent-of-code/src/main/java/calorie_counting/Main.java
+++ java-advent-of-code/src/main/java/calorie_counting/Main.java
@@ -2,32 +2,26 @@ package calorie_counting;
 
 import shared.FileReaderUtil;
 
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> input = new FileReaderUtil().readFile("elf_calories.txt");
+        String input = new FileReaderUtil().readFileAsString("elf_calories.txt");
 
-        Map<Integer, Integer> elfToCalories = new HashMap<>();
-        int idx = 0;
-        for (String row : input) {
-            try {
-                int parsedRow = Integer.parseInt(row);
-                Integer currentCalories = elfToCalories.get(idx);
-                elfToCalories.put(idx, currentCalories != null ? currentCalories + parsedRow : parsedRow);
-            } catch (Exception e) {
-                idx++;
-            }
-        }
+        List<Integer> caloriesPerElf = Arrays.stream(input.split("\n\n"))
+                .map(elfCalories ->
+                        Arrays.stream(elfCalories.split("\n"))
+                                .map(Integer::parseInt)
+                                .reduce(0, (val, tot) -> tot + val))
+                .toList();
 
         System.out.println("Maximum n calorie carriers");
-        System.out.println(topNCaloriesTotal(elfToCalories, 3));
+        System.out.println(topNCaloriesTotal(caloriesPerElf, 3));
     }
 
-    private static int topNCaloriesTotal(Map<Integer, Integer> elfToCalories, int n) {
-        return elfToCalories.values().stream().sorted(Comparator.reverseOrder()).limit(n).reduce(0, (value, acc) -> acc + value);
+    private static int topNCaloriesTotal(List<Integer> caloriesPerElf, int n) {
+        return caloriesPerElf.stream().sorted(Comparator.reverseOrder()).limit(n).reduce(0, (value, acc) -> acc + value);
     }
 }
--- java-advent-of-code/src/main/java/shared/FileReaderUtil.java
+++ java-advent-of-code/src/main/java/shared/FileReaderUtil.java
@@ -7,12 +7,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FileReaderUtil {
 
     public List<String> readFile(String fileName) {
+        return readFileAsStream(fileName).toList();
+    }
+
+    public String readFileAsString(String fileName) {
         try {
-            return Files.lines(getFilePath(fileName)).toList();
+            return Files.readString(getFilePath(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Stream<String> readFileAsStream(String fileName) {
+        try {
+            return Files.lines(getFilePath(fileName));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
